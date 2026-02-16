00f1afb5bb022b50bb1ef78c9dc28ab019c63f9e
J0sH-code <joshrylesanteno07@gmail.com>
2026-02-16 23:58:46 +0800

    Initialize empty classes for future HTTP method handlers (GET, PUT, POST, etc.)

--- src/backend/myServer.java
+++ src/backend/myServer.java
@@ -1,17 +1,47 @@
 package backend;
 
 import com.sun.net.httpserver.*;
-
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class myServer {
-    static boolean running = false;
     public static void main(String[] args) throws IOException {
-        var server = HttpServer.create();
-        server.bind(new InetSocketAddress("192.168.100.110", 3000), 0);
-        server.start();
+        var server = HttpServer.create(new InetSocketAddress(3000), 0);
 
-        http
+        server.createContext("/test", new reqHandler());
+        server.setExecutor(null); 
+        server.start();
+    }
+
+    static class reqHandler implements  HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Server is working";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class getHandler {
+
+    }
+
+    static class postHandler {
+
+    }
+
+    static class patchHandler {
+
+    }
+
+    static class putHandler {
+
+    }
+
+    static class deleteHandler {
+        
     }
 }
